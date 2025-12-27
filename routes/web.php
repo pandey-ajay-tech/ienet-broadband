8d9f36eb70bc650620799908d18de92b2f9461ba
Ajay Pandey <apandey3212@gmail.com>
2025-12-27 12:52:14 +0530

    add asset folder

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WebController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+}); 
+
+
+Route::get('/about', [WebController::class, 'index'])->name('front.about-us');
+Route::get('/contact-us', [WebController::class, 'show'])->name('front.contact-us');
